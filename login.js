0460f19e9ebbea0e63cc936563cc9845284caafc
imrulkayes2 <ikayes.eee@gmail.com>
2026-03-08 16:23:07 +0600

    login form authencation & navigate to another page

--- login.js
+++ login.js
@@ -1,4 +1,14 @@
 document.getElementById('login-btn').addEventListener('click',function(event){
     event.preventDefault();
+    const accNumber=document.getElementById('accNum').value;
+    const accPassword=document.getElementById('password').value;
+    if(accNumber.length==11 && accPassword.length==4){
+        window.location.href="./dashboard.html";
+        // window.open('dashboard.html','_blank')
+    }
+    else{
+        alert('Please insert 11 Digit Number & 4 digit Password')
+    }
+
     
 })
